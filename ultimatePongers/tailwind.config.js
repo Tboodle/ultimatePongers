6a8dad6cd3fd679a1a4d2e50dbe328ef9b79d500
Tboodle <tbernstein10@gmail.com>
2022-04-24 16:33:01 -0400

    initial app

---
+++ ultimatePongers/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bti-orange': '#FE5607',
+        'bti-dark': '#2C2C2C',
+      },
+    },
+  },
+  plugins: [],
+};
