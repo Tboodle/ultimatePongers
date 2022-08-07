9edccc027cb77421a9153d829c840aa501f2585a
Tyler Bernstein <Tbernstein10@gmail.com>
2022-08-07 14:16:39 -0400

    fix toolbar overlap and limit match call to 10

--- src/app/shared/services/match.service.ts
+++ src/app/shared/services/match.service.ts
@@ -5,8 +5,10 @@ import {
   addDoc,
   collection,
   collectionData,
+  collectionGroup,
   Firestore,
 } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { PlayerService } from './player.service';
 
 @Injectable({
@@ -14,28 +16,28 @@ import { PlayerService } from './player.service';
 })
 export class MatchService {
   private matchCollection;
-  constructor(firestore: Firestore, private playerService: PlayerService) {
-    this.matchCollection = collection(firestore, 'matches');
+  constructor(
+    private afs: AngularFirestore,
+    private playerService: PlayerService
+  ) {
+    this.matchCollection = afs.collection<Match>('matches', (ref) =>
+      ref.orderBy('date', 'desc').limit(10)
+    );
   }
 
   public getMatches(): Observable<any> {
-    return collectionData(this.matchCollection).pipe(
+    return this.matchCollection.valueChanges().pipe(
       map((matches: any[]) => {
         return matches.map((match) => {
           return { ...match, date: new Date(match.date?.seconds * 1000) };
         });
-      }),
-      map((matches: Match[]) =>
-        matches.sort(
-          (match1, match2) => match2.date.valueOf() - match1.date.valueOf()
-        )
-      )
+      })
     );
   }
 
   public addMatch(match: Match): any {
     this.playerService.addWin(match.winnerId);
     this.playerService.addLoss(match.loserId);
-    return addDoc(this.matchCollection, { ...match });
+    return this.matchCollection.add(match);
   }
 }
