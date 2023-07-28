224400575345c3a33afca59d77af6ef3c2913485
Tyler Bernstein <tyler.bernstein@bti360.com>
2023-07-27 20:20:51 -0400

    add paging to leaderboard

--- src/app/home-page/components/leaderboard/leaderboard.component.ts
+++ src/app/home-page/components/leaderboard/leaderboard.component.ts
@@ -9,6 +9,8 @@ import { Player } from 'src/app/shared/models/player';
 export class LeaderboardComponent {
   @Input() players: Player[];
   @Output() statNavigationEvent = new EventEmitter<string>();
+  currentPage: number = 1;
+  pageSize: number = 10;
 
   getWinPercentageForPlayer(player: Player): string {
     const totalMatches = player.wins + player.losses;
@@ -18,5 +20,15 @@ export class LeaderboardComponent {
     } else {
       return '-';
     }
+  }
+
+  goToPage(pageNumber: number): void {
+    const maxPage = this.players.length / this.pageSize - 1;
+    this.currentPage = pageNumber < 1 ? 1 : pageNumber > maxPage ? maxPage : pageNumber;
+  }
+
+  filterPlayersForPage(players: Player[]): Player[] {
+    const start = (this.currentPage - 1) * this.pageSize;
+    return players.slice(start, start + this.pageSize);
   }
 }
